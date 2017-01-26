1f9ff9f194066bc78f5ebf0183c7e54042c581b3
Martin Molhov <mmolhov333@gmail.com>
2017-01-26 20:38:24 +0200

    adding some changes

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("GitHub rules the seven seas!");
+            Console.WriteLine("GitHub rules the seven seas! Also I like trains");
         }
     }
 }
